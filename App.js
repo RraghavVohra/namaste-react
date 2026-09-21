1e08643e7cd4b9f374e048c86ff4dd54869dca68
RraghavVohra <raghavvohra1499@gmail.com>
2026-09-21 10:49:37 +0530

    Added episode-2 stuff

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading  = React.createElement("h1",
 { id: "heading", xyz: "abc"},
 "Hello World from React!"
@@ -23,14 +27,14 @@ const parent = React.createElement("div",{ id: "parent" },
     React.createElement("div",{ id : "child" },
     // FOR MAKING SIBLINGS WE WILL USE AN ARRAY
     [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag")
+    React.createElement("h1", { key: "1" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "2" }, "I'm an h2 tag")
     ]),
     React.createElement("div",{ id : "child2" },
     // FOR MAKING SIBLINGS WE WILL USE AN ARRAY
     [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag")
+    React.createElement("h3", { key: "3" }, "I'm an h1 tag"),
+    React.createElement("h4", { key: "4" }, "I'm an h2 tag")
     ])
 );
 
