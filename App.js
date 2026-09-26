357e871c974fe6584e25b3e1e87888d4d8101e10
RraghavVohra <raghavvohra1499@gmail.com>
2026-09-26 16:59:49 +0530

    Added one card

--- App.js
+++ App.js
@@ -23,10 +23,36 @@ const Header = () => {
     );
 };
 
+// RESTAURANT CARD
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style = {{backgroundColor: "#f0f0f0"}}>
+            <img className="res-logo" alt="res-logo" src="https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_600,h_400/v1681884314/008781bada99c992ebf23970c7f2f37a.webp" />
+            <h3>Daisy Mae Cantina</h3>
+            <h4>Mexican Coffee House</h4>
+            <h4>Khan Market, Delhi</h4>
+            <h4>4.0 Stars</h4>
+            <h4>Flat 15% off on pre-booking</h4>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className = "body">
+          <div className="search">Search</div>
+          <div className="res-container">
+            <RestaurantCard /> 
+            </div>
+        </div>
+    );
+};
+
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
+            <Body />
         </div>
     );
 };
