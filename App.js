48b53cb610c672c45e5a1c6be3cef13e84204a99
RraghavVohra <raghavvohra1499@gmail.com>
2026-09-26 15:12:44 +0530

    Added Food App page

--- App.js
+++ App.js
@@ -4,14 +4,36 @@ import ReactDOM from "react-dom/client";
 
 // STARTING FROM SCRATCH
 
-const heading = React.createElement("h1",{id: "heading"},"Namaste React");
+const Header = () => {
+
+    return (
+        <div className="header">
+         <div className="logo-container">
+            <img className="logo" src="https://cdn.dribbble.com/userupload/6738187/file/original-34da7b3d3a9cab40b12c92817eea780f.jpg"/>
+         </div>
+          <div className="nav-items">
+           <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+           </ul>
+          </div>
+       </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    );
+};
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout />);
 
-console.log(heading);
-
-const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
-
-console.log(jsxHeading);
 
