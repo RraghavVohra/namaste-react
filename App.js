bf9c27feb058b826b630b01cde66ff08c11c553c
RraghavVohra <raghavvohra1499@gmail.com>
2026-09-26 09:14:05 +0530

    Added some code

--- App.js
+++ App.js
@@ -2,43 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading  = React.createElement("h1",
-{ id: "heading", xyz: "abc"},
-"Hello World from React!"
-);
-// To create a root, we use React DOM
+// STARTING FROM SCRATCH
+
+const heading = React.createElement("h1",{id: "heading"},"Namaste React");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
 
-// For giving attributes we can use {}
-//  { id: "myHeading", className: "title" }
+console.log(heading);
 
-// MAKING NESTED KIND OF STRUCTURE
-// ReactElement(Object) => HTML(Browser Understands)
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
 
-// <div id="parent">
-//  <div id="child">
-//    <h1>I'm an h1 tag</h1>
-//    <h2>I'm an h2 tag</h2>
-//  </div>
-// </div>
-
-
-const parent = React.createElement("div",{ id: "parent" },
-    React.createElement("div",{ id : "child" },
-    // FOR MAKING SIBLINGS WE WILL USE AN ARRAY
-    [
-    React.createElement("h1", { key: "1" }, "I'm an h1 tag"),
-    React.createElement("h2", { key: "2" }, "I'm an h2 tag")
-    ]),
-    React.createElement("div",{ id : "child2" },
-    // FOR MAKING SIBLINGS WE WILL USE AN ARRAY
-    [
-    React.createElement("h3", { key: "3" }, "I'm an h1 tag"),
-    React.createElement("h4", { key: "4" }, "I'm an h2 tag")
-    ])
-);
-
-console.log(parent);
-
-root.render(parent);
+console.log(jsxHeading);
 
